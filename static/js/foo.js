f3c3c18bcc686c82e9bba76bdbccf83fb3896a29
Will Thompson <will@willthompson.co.uk>
2013-11-16 10:56:44 +0000

    Count months (for Baby Lucas' benefit)

--- static/js/foo.js
+++ static/js/foo.js
@@ -1,6 +1,7 @@
 function startCountdown(selector, arrival, offsetMinutes, yes_url) {
     $(selector).countdown(
       {until: arrival,
+       format: 'odHMS',
        timezone: offsetMinutes,
        expiryUrl: yes_url});
 }
