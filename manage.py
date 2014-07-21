c1c4657cba5c3eab3e1751dafb2a2dc6a4ee4990
Will Thompson <will@willthompson.co.uk>
2014-07-21 06:35:49 +0100

    manage: threaded by default

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 # vim: sts=4 sw=4 et
 import os
 os.environ.setdefault('ESTRAGON_SETTINGS', os.path.join(os.getcwd(), 'settings.py'))
+import sys
 
 from flask.ext.script import Manager, Server
 from estragon import app
@@ -10,5 +11,8 @@ manager = Manager(app)
 manager.add_command('runserver', Server(host='::1'))
 
 if __name__ == "__main__":
+    if '--threaded' not in sys.argv and '--processes' not in sys.argv:
+        sys.argv.append('--threaded')
+
     manager.run()
 
